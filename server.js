7b2c130b6f53c4685e20a3d61712bee971935e6a
Vikesh667 <Vikesh.667kumar@gmail.com>
2023-05-12 12:20:11 +0530

    start

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const cors=require('cors')
 
 const app=express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+
+app.get("/",(req,res)=>{
+    res.send("Home Page")
+})
 const PORT=process.env.PORT || 5000
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
